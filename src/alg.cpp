92b27f0272136006cd009127b2824a3b0e7df484
pivnik1 <niga.gurunov38@mail.ru>
2025-04-08 17:54:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,69 +1,67 @@
 // Copyright 2021 NNTU-CS
 
-int findPairCountBasic(int *numbers, int size, int desiredSum) {
-    int pairCounter = 0;
-    for (int start = 0; start < size; start++) {
-        for (int next = start + 1; next < size; next++) {
-            if (numbers[start] + numbers[next] == desiredSum) {
-                pairCounter++;
+int countPairs1(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
             }
         }
     }
-    return pairCounter;
+    return count;
 }
 
-int findPairCountOptimized(int *numbers, int size, int desiredSum) {
-    int pairCounter = 0;
-    int upperLimit = size - 1;
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
 
-    while (upperLimit > 0 && numbers[upperLimit] > desiredSum) {
-        upperLimit--;
-    }
-
-    for (int base = 0; base < size; base++) {
-        for (int current = upperLimit; current > base; current--) {
-            if (numbers[base] + numbers[current] == desiredSum) {
-                pairCounter++;
-            }
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            count++;
+            int l = arr[left], r = arr[right];
+            while (left < right && arr[left] == l) left++;
+            while (right > left && arr[right] == r) right--;
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
         }
     }
-    return pairCounter;
+
+    return count;
 }
 
-int findPairCountBinary(int *numbers, int size, int desiredSum) {
-    int pairCounter = 0;
+int countPairs3(int *arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
 
-    for (int position = 0; position < size; position++) {
-        int neededValue = desiredSum - numbers[position];
-        int lowerBound = position + 1;
-        int upperBound = size - 1;
-
-        while (lowerBound <= upperBound) {
-            int middle = lowerBound + (upperBound - lowerBound) / 2;
-            if (numbers[middle] >= neededValue) {
-                upperBound = middle - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                int j = mid;
+                while (j >= left && arr[j] == target) {
+                    count++;
+                    j--;
+                }
+                j = mid + 1;
+                while (j <= right && arr[j] == target) {
+                    count++;
+                    j++;
+                }
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
             } else {
-                lowerBound = middle + 1;
+                right = mid - 1;
             }
-        }
-        int startIndex = lowerBound;
-
-        lowerBound = position + 1;
-        upperBound = size - 1;
-
-        while (lowerBound <= upperBound) {
-            int middle = lowerBound + (upperBound - lowerBound) / 2;
-            if (numbers[middle] <= neededValue) {
-                lowerBound = middle + 1;
-            } else {
-                upperBound = middle - 1;
-            }
-        }
-        int endIndex = upperBound;
-
-        if (startIndex <= endIndex) {
-            pairCounter += endIndex - startIndex + 1;
         }
     }
-    return pairCounter;
+
+    return count;
 }
